35c7b720e86b4cf41aefb0abea1553bb001de7d3
fabioabrantes <fabio.abrantes.diniz@gmail.com>
2024-04-30 15:44:32 -0300

    congig deploy

--- src/infra/database/connection.ts
+++ src/infra/database/connection.ts
@@ -3,7 +3,7 @@ import { env } from '../../env';
 
 export const config: Knex.Config = {/* pegamos a tipagem do Knex para o intelisense nos ajudar */
   client: env.DATABASE_CLIENT,
-  connection: env.DATABASE_URL === 'sqlite' ? { filename: env.DATABASE_URL } : env.DATABASE_URL,
+  connection: env.DATABASE_CLIENT === 'sqlite' ? { filename: env.DATABASE_URL } : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
